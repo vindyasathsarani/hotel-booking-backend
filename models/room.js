e553f2c1582dda0ee8f474481eb5aaa7221b3862
Vindya Sathsarani <123439572+vindyasathsarani@users.noreply.github.com>
2024-10-15 09:39:42 +0530

    Created room schema

---
+++ models/room.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema({
+  roomId: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  maxGuests: {
+    type: Number,
+    required: true,
+    default: 3,
+  },
+  available: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  photos: [
+    {
+      type: String,
+    },
+  ],
+  specialDescription: {
+    type: String,
+    default: "",
+  },
+  notes: {
+    type: String,
+    default: "",
+  },
+});
+
+const Room = mongoose.model("Room", roomSchema);
+
+export default Room;
